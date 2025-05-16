bfcbf0615a2e70a34a2b98b9f102e37cec320d44
Timtim2005 <dedeladebrouille555@gmail.com>
2025-05-16 16:49:02 +0200

    Modif sensor.cpp for support of AQI calculations

--- src/model/Sensor.cpp
+++ src/model/Sensor.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 #include "Sensor.h"
 #include "../utils/GPS.h"
+#include <bits/stdc++.h>
 
 Sensor &Sensor::operator=(const Sensor &unSensor)
 {
@@ -51,4 +52,54 @@ void Sensor::banSensor()
 bool Sensor::checkDistance(double latitude, double longitude, double radius)
 {
     return distance(this->latitude, this->longitude, latitude, longitude) <= radius;
+}
+
+float Sensor::calculateAirQuality(time_t startTime, time_t endTime, List<Measurement> measurements)
+{
+    float totalPM10 = 0;
+    float totalNO2 = 0;
+    float totalO3 = 0;
+    float totalSO2 = 0;
+
+    int countPM10 = 0;
+    int countNO2 = 0;
+    int countO3 = 0;
+    int countSO2 = 0;
+
+    for (Measurement measurement : measurements)
+    {
+        if (measurement.getTime() >= startTime && measurement.getTimestamp() <= endTime)
+        {
+            string attributeType = measurement.getAttribute();
+            switch (attributeType)
+            {
+            case "PM10":
+                totalPM10 += measurement.getValue();
+                break;
+
+            case "NO2":
+                totalNO2 += measurement.getValue();
+                break;
+
+            case "O3":
+                totalO3 += measurement.getValue();
+                break;
+
+            case "SO2":
+                totalSO2 += measurement.getValue();
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+    float averagePM10 = (countPM10 > 0) ? ((totalPM10 / countPM10) * 50 / 35) : 0;
+    float averageNO2 = (countNO2 > 0) ? ((totalNO2 / countNO2) * 50 / 213) : 0;
+    float averageO3 = (countO3 > 0) ? ((totalO3 / countO3) * 50 / 82) : 0;
+    float averageSO2 = (countSO2 > 0) ? ((totalSO2 / countSO2) * 50 / 200) : 0;
+
+    return max(averagePM10, max(averageNO2, max(averageO3, averageSO2)));
+}
+{
 }
