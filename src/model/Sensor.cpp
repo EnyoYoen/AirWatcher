f2cbbb525cc7ea46f5626ceb9dffb20e94dbaeeb
EnyoYoen <enyotoyrep@gmail.com>
2025-05-16 15:22:50 +0200

    Add Sensor class and tests/, ui/, utils/ subfolders

--- src/model/Sensor.cpp
+++ src/model/Sensor.cpp
@@ -1,74 +1,77 @@
 /*************************************************************************
-                           Xxx  -  description
+                           Sensor  -  description
                              -------------------
-    début                : $DATE$
-    copyright            : (C) $YEAR$ par $AUTHOR$
-    e-mail               : $EMAIL$
+    début                : 16/5/2025
+    copyright            : (C) 2025 par Garoux, Hequet, McCurdy, Lafon de la Laurencie, Peyrot.
 *************************************************************************/
 
-//---------- Réalisation de la classe <Xxx> (fichier Xxx.cpp) ------------
-
-//---------------------------------------------------------------- INCLUDE
-
-//-------------------------------------------------------- Include système
 using namespace std;
+
 #include <iostream>
+#include <cmath>
 
-//------------------------------------------------------ Include personnel
-#include "Xxx.h"
-
-//------------------------------------------------------------- Constantes
-
-//----------------------------------------------------------------- PUBLIC
-
-//----------------------------------------------------- Méthodes publiques
-// type Xxx::Méthode ( liste des paramètres )
-// Algorithme :
-//
-//{
-//} //----- Fin de Méthode
+#include "Sensor.h"
 
 
-//------------------------------------------------- Surcharge d'opérateurs
-Xxx & Xxx::operator = ( const Xxx & unXxx )
-// Algorithme :
-//
+Sensor & Sensor::operator = ( const Sensor & unSensor )
 {
-} //----- Fin de operator =
+    if ( this != &unSensor )
+    {
+        this->sensorId = unSensor.sensorId;
+        this->latitude = unSensor.latitude;
+        this->longitude = unSensor.longitude;
+        this->reliable = unSensor.reliable;
+    }
+}
 
 
-//-------------------------------------------- Constructeurs - destructeur
-Xxx::Xxx ( const Xxx & unXxx )
-// Algorithme :
-//
+Sensor::Sensor ( const Sensor & unSensor )
 {
-#ifdef MAP
-    cout << "Appel au constructeur de copie de <Xxx>" << endl;
-#endif
-} //----- Fin de Xxx (constructeur de copie)
+    this->sensorId = unSensor.sensorId;
+    this->latitude = unSensor.latitude;
+    this->longitude = unSensor.longitude;
+    this->reliable = unSensor.reliable;
+}
 
 
-Xxx::Xxx ( )
-// Algorithme :
-//
+Sensor::Sensor ( string sensorId, double latitude, double longitude )
 {
-#ifdef MAP
-    cout << "Appel au constructeur de <Xxx>" << endl;
-#endif
-} //----- Fin de Xxx
+    this->sensorId = sensorId;
+    this->latitude = latitude;
+    this->longitude = longitude;
+    this->reliable = true;
+}
 
 
-Xxx::~Xxx ( )
-// Algorithme :
-//
+Sensor::~Sensor ( )
 {
-#ifdef MAP
-    cout << "Appel au destructeur de <Xxx>" << endl;
-#endif
-} //----- Fin de ~Xxx
 
+}
 
-//------------------------------------------------------------------ PRIVE
+void Sensor::banSensor ( )
+{
+    this->reliable = false;
+}
 
-//----------------------------------------------------- Méthodes protégées
+bool Sensor::checkDistance (double latitude, double longitude, double radius)
+{
+    return distance(this->latitude, this->longitude, latitude, longitude) <= radius;
+}
 
+double Sensor::distance ( double lat1, double lon1, double lat2, double lon2 )
+{
+    // Haversine formula to calculate the distance between two points on the Earth
+    const double R = 6371.0;
+    double dLat = toRadians(lat2 - lat1);
+    double dLon = toRadians(lon2 - lon1);
+    double a = sin(dLat / 2) * sin(dLat / 2) +
+               cos(toRadians(lat1)) * cos(toRadians(lat2)) *
+               sin(dLon / 2) * sin(dLon / 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    return R * c;
+}
+
+double Sensor::toRadians ( double degree )
+{
+    return degree * M_PI / 180.0;
+}
