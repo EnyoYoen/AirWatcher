e7969fa7d90ea0aa83d20fc7539643f9e041bd8a
GarouxE <133769796+GarouxE@users.noreply.github.com>
2025-05-16 17:17:20 +0200

    Update PrivateUser.h

--- src/model/PrivateUser.h
+++ src/model/PrivateUser.h
@@ -1,4 +1,3 @@
-
 /*************************************************************************
                            PrivateUser  -  description
                              -------------------
@@ -7,83 +6,77 @@
     e-mail               : ---
 *************************************************************************/
 
-//---------- Interface de la classe <PrivateUser> (fichier PrivateUser.h) ----------------
-#if ! defined ( PRIVATE_USER_H )
-#define PRIVATE_USER_H
+//---------- Réalisation de la classe <PrivateUser> (fichier PrivateUser.cpp) ------------
 
-//--------------------------------------------------- Interfaces utilisées
-#include "User.h"
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include <string>
+#include "PrivateUser.h"
+
 //------------------------------------------------------------- Constantes
 
-//------------------------------------------------------------------ Types
-
-//------------------------------------------------------------------------
-// Rôle de la classe <PrivateUser>
-//
-//
-//------------------------------------------------------------------------
-
-class PrivateUser : public User
-{
 //----------------------------------------------------------------- PUBLIC
 
-public:
 //----------------------------------------------------- Méthodes publiques
-    // type Méthode ( liste des paramètres );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
+// type PrivateUser::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+
+//------------------------------------------------- Surcharge d'opérateurs
 
 
 //-------------------------------------------- Constructeurs - destructeur
-    PrivateUser (int id,  std::string pwd );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
 
-    virtual ~PrivateUser ( );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-    bool isReliable() const;
-    // Mode d'emploi :
-    // Accéder à 'reliable'
-    // Contrat :
-    //
+bool PrivateUser::isReliable() const
+{
+    return reliable;
+}
 
 
-    void setReliable(bool value);
-    // Mode d'emploi :
-    // Accéder à 'reliable'
-    // Contrat :
-    //
+void PrivateUser::setReliable(bool value)
+{
+    reliable = value;
+}
 
 
-    int getPoints();
-    // Mode d'emploi :
-    // Accéder à 'points'
-    // Contrat :
-    //
-    void givePoints(int pts);
-    // Mode d'emploi :
-    // Ajouter ou enlever des points
-    // Contrat :
-    //
+void PrivateUser::givePoints(int pts)
+{
+    points += pts;
+}
+
+int PrivateUser::getPoints()
+{
+    return points;
+}
+
+PrivateUser::PrivateUser (int id, string passwd)
+// Algorithme :
+: User(id, passwd), reliable(true), points(0)
+{
+#ifdef MAP
+cout << "Appel au constructeur de <PrivateUser>" << endl;
+#endif
+}
+
+
+PrivateUser::~PrivateUser ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <PrivateUser>" << endl;
+#endif
+} //----- Fin de ~PrivateUser
+
 
 //------------------------------------------------------------------ PRIVE
 
-protected:
 //----------------------------------------------------- Méthodes protégées
-
-//----------------------------------------------------- Attributs protégés
-    bool reliable;
-    int points;
-};
-
-//-------------------------------- Autres définitions dépendantes de <Xxx>
-
-#endif // PRIVATE_USER_H
