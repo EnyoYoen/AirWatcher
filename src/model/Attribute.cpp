83283f28a9d9f33530341b7ff72dcfebe2a700b1
GarouxE <133769796+GarouxE@users.noreply.github.com>
2025-05-16 17:50:26 +0200

    Update Attribute.cpp

---
+++ src/model/Attribute.cpp
@@ -0,0 +1,78 @@
+/*************************************************************************
+                           Attribute  -  description
+                             -------------------
+    début                : 16/02/2025
+    copyright            : (C) 2025 par Ewan
+    e-mail               : ---
+*************************************************************************/
+
+//---------- Réalisation de la classe <Attribute> (fichier Attribute.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+#include <string>
+
+
+//------------------------------------------------------ Include personnel
+#include "Attribute.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+
+
+//-------------------------------------------- Constructeurs - destructeur
+
+
+string Attribute::getAttributeId()
+{
+    return attributeId;
+}
+
+string Attribute::getUnit()
+{
+    return unit;
+}
+
+string Attribute::getDescription()
+{
+    return description;
+}
+
+Attribute::Attribute (string unAttributeId, string unUnit, string uneDescription )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Attribute>" << endl;
+#endif
+
+    attributeId = unAttributeId;
+    unit = unUnit;
+    description = uneDescription;
+} //----- Fin de Attribute
+
+
+Attribute::~Attribute ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Attribute>" << endl;
+#endif
+} //----- Fin de ~Attribute
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
