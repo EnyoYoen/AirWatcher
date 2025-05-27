1b8141b50c97bb9102e21b9af9ff916621d13fc4
erwannHequet <futerwann@gmail.com>
2025-05-27 11:52:16 +0200

    add test to interact with the menu

--- src/tests/test_airWatcher.cpp
+++ src/tests/test_airWatcher.cpp
@@ -1,9 +1,22 @@
 #include <gtest/gtest.h>
 #include "../services/AirWatcher.h"
+#include <sstream>
+#include <iostream>
 
+// Voilà un exemple d'utilisation pour manipuler le menu
 TEST(TestAirWatcher, quitterDirectement) {
-    //AirWatcher aw;
+    
+    std::istringstream input("2\n");
 
-    // Ça affiche le menu
-    //cout << 2;
+    // Sauvegarde cin original
+    auto cin_backup = std::cin.rdbuf();
+
+    // Redirige cin vers notre input simulé
+    std::cin.rdbuf(input.rdbuf());
+
+    // Appelle la fonction qui attend l'entrée standard
+    AirWatcher aw;
+
+    // Restaure cin
+    std::cin.rdbuf(cin_backup);
 }
